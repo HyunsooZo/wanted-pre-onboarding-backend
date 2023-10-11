a20872673f3b58d288b87f929011c08b6c91e214
HyunsooZo <bzhs1992@icloud.com>
2023-10-11 19:54:32 +0900

    Chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/wanted/WantedPreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedPreAssignmentApplication.class, args);
+    }
+
+}
