b7b97ec090a486a56b71935637fa279092637c6f
HyunsooZo <bzhs1992@icloud.com>
2023-10-12 04:56:49 +0900

    Feat: 커스텀 Exception 파일 추가
    CustomException- 특정 예외 상황을 나타내기 위해 생성된 클래스
    ErrorCode- 예외 상황에 대한 오류 코드를 정의한 클래스
    ErrorResponse- 클라이언트에게 예외 상황에 대한 응답을 반환하기 위한 클래스
    GlobalExceptionHandler- 전역예외처리 클래스

---
+++ src/main/java/com/wanted/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.wanted.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    // 공통 Exception
+    UNDEFINED_EXCEPTION(HttpStatus.BAD_REQUEST, "알 수 없는 예외가 발생했습니다."),
+
+    // 회사 Exception
+    COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회사입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
---
+++ src/main/java/com/wanted/exception/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.wanted.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.wanted.exception.ErrorCode.UNDEFINED_EXCEPTION;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidException(
+            MethodArgumentNotValidException exception) {
+
+        List<String> errors = new ArrayList<>();
+        exception.getBindingResult().getAllErrors()
+                .forEach(error -> errors.add(error.getDefaultMessage()));
+
+        return ResponseEntity.status(BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorCode(exception.getErrorCode())
+                .message(exception.getMessage())
+                .build();
+
+        log.error(exception.getMessage(), exception);
+
+        return ResponseEntity
+                .status(errorResponse.getErrorCode().getStatus())
+                .body(errorResponse);
+    }
+
+    //별도로 정의되지 않았으나 발생한 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> Exception(Exception exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(exception.getMessage())
+                .errorCode(UNDEFINED_EXCEPTION)
+                .build();
+
+        log.error(exception.getMessage(), exception);
+
+        return ResponseEntity
+                .status(errorResponse.getErrorCode().getStatus())
+                .body(errorResponse);
+    }
+}
