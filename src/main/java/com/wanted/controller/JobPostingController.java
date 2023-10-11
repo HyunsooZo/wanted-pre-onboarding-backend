ea6884ec58ab867982f46102013e9c31c3763c43
HyunsooZo <bzhs1992@icloud.com>
2023-10-12 06:16:26 +0900

    Feat: 전체 채용공고 조회 기능 추가

--- src/main/java/com/wanted/controller/JobPostingController.java
+++ src/main/java/com/wanted/controller/JobPostingController.java
@@ -6,14 +6,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+import java.util.List;
+
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/job-postings")
@@ -31,6 +31,17 @@ public class JobPostingController {
         jobPostingService.addJobPosting(jobPostingRequestDto);
 
         return ResponseEntity.status(CREATED).build();
+    }
+
+    @GetMapping
+    @ApiOperation(value = "채용공고 조회", notes = "채용공고를 조회합니다.")
+    public ResponseEntity<JobPostingDto.PostingResponse> getJobPostings() {
+
+        List<JobPostingDto> jobPostingResponseDto =
+                jobPostingService.getJobPostings();
+
+        return ResponseEntity.status(OK)
+                .body(JobPostingDto.PostingResponse.from(jobPostingResponseDto));
     }
 
 }
