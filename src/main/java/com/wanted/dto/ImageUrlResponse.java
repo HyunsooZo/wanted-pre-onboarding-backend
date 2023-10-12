bb680d390dd1dae9c3ec08e3b57ff1c26c025904
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-10-13 07:05:36 +0900

    Merge: 이미지 업로드/삭제 기능 추가 (#7)
    * Feat: 채용공고 정보 수정하는 기능 추가
    
    수정이 가능한 모든 필드 중 null이거나 (String일경우) ""(빈값) 이
    아닌 경우에만 해당 필드를 업데이트 하고 저장.
    
    * Test: 채용공고 상세정보보기 테스트코드 추가
    
    testModifyJobPosting_success() -> 수정 성공
    
    * Chore: 채용공고 이미지 업로드 및 회사 이미지 업로드를 위한 S3 의존성 추가
    
    * Feat: 이미지 업로드를 구현하기 위한 S3Config 추가
    
    * Feat: 이미지 업로드 기능 추가

---
+++ src/main/java/com/wanted/dto/ImageUrlResponse.java
@@ -0,0 +1,14 @@
+package com.wanted.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageUrlResponse {
+    private String imageUrl;
+}
